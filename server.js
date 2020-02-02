6f97912b0a507fe7b0eead23fe7fa5ffa52ae3e8
UI JDeveloper <ulvi.ibadov@gmail.com>
2020-02-03 01:07:54 +0400

    Added routes

--- server.js
+++ server.js
@@ -2,12 +2,23 @@ const express = require("express");
 const chalk = require("chalk");
 const connectDB = require("./config/db");
 
+const userRoute = require("./routes/api/users");
+const authRoute = require("./routes/api/auth");
+const postsRoute = require("./routes/api/posts");
+const profileRoute = require("./routes/api/profile");
+
 const app = express();
 connectDB();
 
 app.get("/", (req, res) => {
   res.send("CONNECTED");
 });
+
+// DEFINE ROUTES
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/posts", postsRoute);
+app.use("/api/profile", profileRoute);
 
 const PORT = process.env.PORT || 5000;
 
